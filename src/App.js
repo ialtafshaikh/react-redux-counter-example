dda17bc585751a4b1f0655dd9d0c176f3b20c53b
ialtafshaikh <altaf.shaikh@contentstack.com>
2021-01-04 17:05:57 +0530

    connected the counter component with the store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Counter from "./Components/Counter";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <header className="App-header">
+          <Counter />
+        </header>
+      </Provider>
     </div>
   );
 }
